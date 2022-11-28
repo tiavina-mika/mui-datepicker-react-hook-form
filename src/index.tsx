ff73070fce05e2197ec5747122e6739a3886c356
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2022-11-28 16:15:13 +0300

    fix import

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/material/styles';
-import Demo from './demo';
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import { StyledEngineProvider } from "@mui/material/styles";
+import Form from "./Form";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      <Demo />
+      <Form />
     </StyledEngineProvider>
   </React.StrictMode>
 );
